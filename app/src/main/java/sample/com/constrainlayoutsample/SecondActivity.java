639fb0de1210546320cc64d59c673a194a4dbb28
akhilbv1 <akhilbv1@gmail.com>
2019-04-03 13:42:03 +0530

    Initial commit

---
+++ app/src/main/java/sample/com/constrainlayoutsample/SecondActivity.java
@@ -0,0 +1,6 @@
+package sample.com.constrainlayoutsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/sample/com/constrainlayoutsample/ThirdActivity.java
@@ -0,0 +1,6 @@
+package sample.com.constrainlayoutsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThirdActivity extends AppCompatActivity {
+}
